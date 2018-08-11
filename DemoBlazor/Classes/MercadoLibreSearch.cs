fa27e67283c3d5da40c7b5506ec3ae1f63058173
MaTSENNIN <cavalcante.matheusveiga@gmail.com>
2018-08-10 23:39:46 -0300

    Blazor Sample

---
+++ DemoBlazor/Classes/MercadoLibreSearch.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Components;
+using Microsoft.AspNetCore.Blazor.Services;
+
+namespace DemoBlazor.Classes
+{
+    public class MercadoLibreSearch
+    {
+        public MercadoLibreSearch()
+        {
+            
+        }
+
+        public string Ola()
+        {
+            return "Olá!";
+        }
+    }
+}
